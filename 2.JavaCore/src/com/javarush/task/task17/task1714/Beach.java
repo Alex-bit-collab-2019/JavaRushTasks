2ac5f82eb9026a4a7c5494e895b19dcd92d8de15
Aleksandr <avsokoloww@gmail.com>
2021-10-13 19:01:15 +0200

    task 1714 Comparable <Beach>

--- 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1714/Beach.java
@@ -4,7 +4,7 @@ package com.javarush.task.task17.task1714;
 Comparable
 */
 
-public class Beach {
+public class Beach implements  Comparable <Beach>{
     private String name;      //название
     private float distance;   //расстояние
     private int quality;    //качество
@@ -15,31 +15,53 @@ public class Beach {
         this.quality = quality;
     }
 
-    public String getName() {
+    public synchronized String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public synchronized void setName(String name) {
         this.name = name;
     }
 
-    public float getDistance() {
+    public synchronized float getDistance() {
         return distance;
     }
 
-    public void setDistance(float distance) {
+    public synchronized void setDistance(float distance) {
         this.distance = distance;
     }
 
-    public int getQuality() {
+    public synchronized int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    public synchronized void setQuality(int quality) {
         this.quality = quality;
     }
 
     public static void main(String[] args) {
 
+        Beach beach1 = new Beach ("Palm Beach", 50, 5);
+        Beach beach2 = new Beach ("Miami Beach", 100, 4);
+        Beach beach3 = new Beach ("Deerfield Beach", 100, 4);
+        Beach beach4 = new Beach ("Miami II", 25, 4);
+
+        System.out.println(beach1.compareTo(beach2));
+        System.out.println(beach2.compareTo(beach3));
+        System.out.println(beach3.compareTo(beach4));
+        System.out.println(beach1.compareTo(beach4));
+
+        System.out.println(beach2.compareTo(beach1));
+        System.out.println(beach3.compareTo(beach2));
+        System.out.println(beach4.compareTo(beach3));
+        System.out.println(beach4.compareTo(beach1));
+
     }
+
+    @Override
+    public synchronized int compareTo(Beach o) {
+        int result = Float.compare(o.getDistance(), getDistance()) + Integer.compare(getQuality(), o.getQuality());
+        return result;
+    }
+
 }
