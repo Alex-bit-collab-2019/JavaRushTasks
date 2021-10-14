5a91cc58471043f0393872816cc40e1f39f13b49
Aleksandr <avsokoloww@gmail.com>
2021-10-14 16:06:58 +0200

    task 18 начало задач 8 уровня

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        
+        try (FileInputStream fis = new FileInputStream(new BufferedReader(new InputStreamReader(System.in)).readLine());) {
+            //.........................
+        }
+        
+        
+    }
+}
