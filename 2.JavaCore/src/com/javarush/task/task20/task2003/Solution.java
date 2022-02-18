682ebc0b77e06db296bfa3970e4e93c1851aeec3
Aleksandr <avsokoloww@gmail.com>
2022-02-18 15:18:53 +0300

    task 20 18-02-22 properties

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/* 
+Знакомство с properties
+*/
+
+public class Solution {
+
+    public static Map<String, String> runtimeStorage = new HashMap<>();
+
+    public static void save(OutputStream outputStream) throws Exception {
+        //напишите тут ваш код
+        Properties properties = new Properties();
+        for (Map.Entry <String, String> entry : runtimeStorage.entrySet()){
+            properties.put(entry.getKey(), entry.getValue());
+        }
+        properties.store(outputStream, "my properties");
+    }
+
+    public static void load(InputStream inputStream) throws IOException {
+        //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.load(inputStream);
+
+        for(Map.Entry entry : properties.entrySet()){
+            runtimeStorage.put(entry.getKey().toString(), entry.getValue().toString());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileInputStream fos = new FileInputStream(reader.readLine())) {                    //path file
+            load(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(runtimeStorage);
+    }
+}
